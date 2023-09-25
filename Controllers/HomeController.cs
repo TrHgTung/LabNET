c076404d1b4a6ef7ef43c17cf9b26f499fac252f
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-25 11:10:09 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using LabNET.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LabNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<ListResponseModel> listResponse = new List<ListResponseModel>()
+            {
+                new ListResponseModel
+                {
+                    Name = "Nguyen Van A",
+                    Email = "mail@mail.com",
+                    Phone = "012345678",
+                    WillAttend = true
+                },
+                new ListResponseModel
+                {
+                    Name = "Nguyen Van B",
+                    Email = "mail2@mail.com",
+                    Phone = "012345679",
+                    WillAttend = false
+                },
+                new ListResponseModel
+                {
+                    Name = "Nguyen Van C",
+                    Email = "mail3@mail.com",
+                    Phone = "012345677",
+                    WillAttend = true
+                },
+            };
+            ViewBag.List = listResponse;
+            ViewData["list"] = listResponse;
+
+            return View(listResponse);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+    }
+}
---
+++ Models/ListResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LabNET.Models
+{
+    public class ListResponseModel
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
